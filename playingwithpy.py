1b313f51e1f34c45b5776e8196a1b0608b33ef46
Shyann Francis <60398934+sfrancis2@users.noreply.github.com>
2022-06-14 18:32:14 -0500

    Add files via upload

---
+++ playingwithpy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+author: Shyann Francis
+
+application: creating a vault server, sending a secret, retireving the secret
+"""
+
+import subprocess
+
+p1 = subprocess.run('vault server -dev', shell=True, capture_output=True)
+p2 = subprocess.run('vault kv put secret/hello foo=world', shell=True, capture_output=True)
+p3 = subprocess.run('vault kv get secret/hello', shell=True, capture_output=True)
+print(p1.stdout, p2.stdout)
+print('complete')
