8dbfb591cd68fc10b7850f8604ce88a25687ba58
Shyann Francis <60398934+sfrancis2@users.noreply.github.com>
2022-07-07 14:21:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import hvac
+import sys
+
+# establish connection
+client = hvac.Client(
+    url='http://127.0.0.1:8200',
+    token='dev-only-token',
+)
+
+# create secret
+create_response = client.secrets.kv.v2.create_or_update_secret(
+    path='my-secret-password',
+    secret=dict(password='Hashi123'),
+)
+print('Secret written successfully.')
+
+# retrieve secret
+read_response = client.secrets.kv.read_secret_version(path='my-secret-password')
+password = read_response['data']['data']['password']
+if password != 'Hashi123':
+    sys.exit('unexpected password')
+
+print(password)
+
+print('Access granted!')
